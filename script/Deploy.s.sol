861bcbd0e7cc2af6a50594a9723d32e6388d74d4
Temi <temiw3@gmail.com>
2025-11-24 00:09:15 +0000

    editted the deployed contract to use eth usd and not btc usd redeployed the contract to sepolia testnet

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -10,9 +10,9 @@ contract Deploy is Script {
     address constant TELLOR_ADDRESS =
         0xB19584Be015c04cf6CFBF6370Fe94a58b7A38830;
 
-    // BTC/USD query ID for Tellor (SpotPrice query for BTC/USD)
-    bytes32 constant BTC_USD_QUERY_ID =
-        keccak256(abi.encode("SpotPrice", abi.encode("btc", "usd")));
+    // ETH/USD query ID for Tellor (SpotPrice query for ETH/USD)
+    bytes32 constant ETH_USD_QUERY_ID =
+        keccak256(abi.encode("SpotPrice", abi.encode("eth", "usd")));
 
     function run() external {
         deployPriceAlert();
@@ -23,14 +23,14 @@ contract Deploy is Script {
 
         PriceAlert priceAlert = new PriceAlert(
             TELLOR_ADDRESS,
-            BTC_USD_QUERY_ID
+            ETH_USD_QUERY_ID
         );
 
         vm.stopBroadcast();
 
         console.log("PriceAlert deployed at:", address(priceAlert));
         console.log("Tellor Address:", TELLOR_ADDRESS);
-        console.log("Query ID:", vm.toString(BTC_USD_QUERY_ID));
+        console.log("Query ID:", vm.toString(ETH_USD_QUERY_ID));
 
         return priceAlert;
     }
